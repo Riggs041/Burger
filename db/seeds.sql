5bccb0bca5665f9e51493eec04095718a12533f8
riggs <riggs041@gmail.com>
2018-07-25 17:40:45 -0500

    initial push

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+-- In this file, write insert queries to populate the burgers table with at least three entries.
+
+INSERT INTO burgers (burger_name) VALUES ('California Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', true);
