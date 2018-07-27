13190bc293512d803ee93fb7aadbd6c6974c042f
riggs <riggs041@gmail.com>
2018-07-27 01:06:00 -0500

    updated files

--- config/orm.js
+++ config/orm.js
@@ -8,3 +8,35 @@ var connection = require("../config/connection.js");
 // updateOne()
 
 // Export the ORM object in module.exports.
+
+var orm = {
+    selectAll: function (cb) {
+        connection.query("SELECT * FROM burgers", function (err, res) {
+            if (err) throw err;
+            cb(res);
+        });
+    },
+
+    insertOne: function (burger_name, cb) {
+        connection.query("INSERT INTO burgers SET ?", {
+            burger_name: burger_name,
+            devoured: false,
+        }, function (err, res) {
+            if (err) throw err;
+            cb(res);
+
+        });
+    },
+
+    updateOne: function(burgerID, cb) {
+        connection.query("UPDATE burgers SET ? WHERE ?", [{devoured: true}, {id: burgerID}],
+        function(err, res) {
+            if (err) throw err;
+            cb(res);
+        });
+    }
+
+};
+
+// Export the orm object for the model (burger.js).
+module.exports = orm;
